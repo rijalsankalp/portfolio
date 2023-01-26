4e870fd5a000cf23d7609570beb09328cb45386a
rijalsankalp <tha075bct039@tcioe.edu.np>
2023-01-26 19:19:08 +0545

    Portfolioed

---
+++ components/Projects.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import Project1 from '../public/assets/projects/project.jpg';
+import slaabur from '../public/assets/projects/slaabur.jpg';
+import FitnessFlow from '../public/assets/projects/fitnessflow.jpg';
+import hdcr from '../public/assets/projects/hdcr.jpg';
+import SnakeAndLadder from '../public/assets/projects/snakeandladder.jpg';
+
+import ProjectItem from './ProjectItem';
+
+const Projects = () => {
+  return (
+    <div id='projects' className='w-full p-2 py-16'>
+      <div className='max-w-[1240px] mx-auto px-2 py-8'>
+        <p className='text-xl tracking-widest uppercase text-[#5651e5]'>
+          Projects
+        </p>
+        <h2 className='py-4'>What I&apos;ve accomplished</h2>
+        <div className='grid md:grid-cols-2 gap-8'>
+          <ProjectItem
+            title='Monaural Multispeaker Speech Separation'
+            backgroundImg={Project1}
+            projectUrl='https://github.com/rijalsankalp/perceparator'
+            tech='Pyhton, Pytorch, Numpy'
+          />
+          <ProjectItem
+            title='Handwritten Devanagari Character Recognition'
+            backgroundImg={hdcr}
+            projectUrl='https://github.com/rijalsankalp/Handwritten-Devanagari-Character-Recognition'
+            tech='Python, Tensorflow, Flask'
+
+          />
+          <ProjectItem
+            title='FitnessFlow'
+            backgroundImg={FitnessFlow}
+            projectUrl='https://github.com/rijalsankalp/FitnessFlow'
+            tech='Next.js, Javascript, Tailwind'
+
+          />
+          <ProjectItem
+            title='Slaabur: History Matters'
+            backgroundImg={slaabur}
+            projectUrl='https://github.com/rijalsankalp/Slaabur'
+            tech='React, Javascript, Tailwind'
+
+          />
+          <ProjectItem
+            title='Snake and Ladder: 3D'
+            backgroundImg={SnakeAndLadder}
+            projectUrl='https://github.com/rijalsankalp/snake-and-ladder'
+            tech='C++, OpenGL, GLUT'
+
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
---
+++ pages/index.js
@@ -0,0 +1,54 @@
+import Head from 'next/head'
+import About from '../components/About'
+import Navbar from '../components/Navbar'
+import Main from '../components/Main'
+import Contact from '../components/Contact'
+import Projects from '../components/Projects'
+import Skills from '../components/Skills'
+
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Sankalpa | ML-Einthusiast</title>
+        <meta name="description" content="I’m a ML/AI einthusiast with occosaional software developing experience." />
+        <link rel="icon" href="/fav.png" />
+      </Head>
+    <Navbar />
+    <Main />
+    <About />
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <Skills />
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    
+    <Projects />
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <Contact />
+    </div>
+  )
+}
