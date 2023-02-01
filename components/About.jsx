349f00f0575f606e10003abed01e7abdc8e0d662
rijalsankalp <tha075bct039@tcioe.edu.np>
2023-02-01 09:44:14 +0545

    Image fix

--- components/About.jsx
+++ components/About.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
 
 const About = () => {
   return (
@@ -31,7 +32,7 @@ const About = () => {
           </Link>
         </div>
         <div className="w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300">
-          <img
+          <Image
             src="../public/assets/about.jpg"
             className="rounded-xl"
             alt="Hello! I am Sankalpa."
